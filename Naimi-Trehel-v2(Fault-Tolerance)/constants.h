75b9e7902ec9402e03232eddcf22b3a8838f0790
alan-lira <alan_lira@id.uff.br>
2019-10-01 04:09:12 -0300

    Adding TWAIT and TELEC constants.

--- Naimi-Trehel-v2(Fault-Tolerance)/constants.h
+++ Naimi-Trehel-v2(Fault-Tolerance)/constants.h
@@ -1,4 +1,6 @@
 #define ELECTED_NODE 0 // Node eleito, inicialmente, como o TOKEN OWNER.
+#define TWAIT 2 // Estimativa (em segundos) do atraso máximo para a presunção de falha.
+#define TELEC 4 // Estimativa (em segundos) do atraso máximo para realizar broadcasting (envio de uma pergunta e recebimento de respostas).
 #define TAG_IDLE 0 // Este node não vai mais solicitar acesso à CRITICAL SECTION e deseja finalizar a sua execução.
 #define TAG_REQUEST 1 // Este node solicitou acesso à CRITICAL SECTION.
 #define TAG_TOKEN 2 // Este node recebeu o TOKEN para acessar a CRITICAL SECTION.
